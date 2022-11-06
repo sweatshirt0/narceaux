443ca29fe33ac25580e9dc275ac2a769fe5e3248
sweatshirt0 <sweatshirttt@proton.me>
2022-11-06 16:11:28 -0500

    contact page.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom"
 import Home from './components/Home'
+import Contact from './components/Contact'
 
 function App() {
   return (
 	<Routes>
-		<Route path="/" element={<Home />} /> 
+		<Route path="/" element={<Home />} />
+		<Route path="/contact" element={<Contact />} />
 	</Routes>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Title from './Title'
+import Nav from './Nav'
+
+const Contact = () => {
+	return(
+		<div className="whole-wrapper">
+			<Title />
+			<Nav />
+			<div className="contact-wrapper">
+				<p className="contact">You can find me on irc servers libcasa and libera as sweatshirt; or less occasionally as samsepi0l.</p>
+			</div>
+		</div>
+	)
+}
+
+export default Contact

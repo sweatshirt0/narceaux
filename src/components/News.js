b4224a0bd9e56731a4515f1ce3c7a825291e8838
sweatshirt0 <sweatshirttt@proton.me>
2022-11-07 21:14:49 -0500

    first news update, plans and news font change.

--- src/components/News.js
+++ src/components/News.js
@@ -11,7 +11,7 @@ const News = () => {
 				<div className="news-title-wrapper">
 					<h2 className="news-title">News</h2>
 				</div>
-				<p className="news">News will go here</p>
+				<p className="news"><span>Update 1 and plans for the future:</span> I have created the base pages -- and thus the bases for my website; this will be basic information for now. In the future; i hope to have a functional chat application within the site, host an irc server related to the site and its online chat, and to find a way to upload my atari-recoded browser game. This will all be done by me - so timing may vary. Thanks for stopping by.</p>
 			</div>
 		</div>
 	)
